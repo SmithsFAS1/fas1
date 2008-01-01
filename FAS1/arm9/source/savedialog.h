4fcb16211d16cbe8fe777f9724aa725ec20ecd9a
chodesmokr <chodesmokr@2cea06f9-9742-0410-a672-5142589a767e>
2008-01-01 21:19:57 +0000

    Added SaveDialog, toggle between dialogs with "Y" for ROM flashing -> SRAM flashing

---
+++ FAS1/arm9/source/savedialog.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <dialog.h>
+#include <label.h>
+#include <button.h>
+#include <imagebutton.h>
+
+#define NUM_BUTTONS 6
+#define MAX_ITEMS 32
+
+void BackupSRAM();
+void WriteSRAM(const char* filename);
+void vBlank();
+
+struct SaveItem
+{
+//	Filetype filetype;
+	char title[256]; //longer for GBA ROM name
+
+	char* address;
+};	
+
+class SaveDialog : public FwGui::Dialog
+{
+public:
+	SaveDialog();
+	virtual ~SaveDialog();
+
+	void ScanSlot1();
+	void RefreshSlot1Buttons();
+	void wait ( int seconds );
+	virtual void ControlClicked(FwGui::Control* control);
+	virtual void KeyUp();
+	virtual void KeyDown();
+	virtual void KeyLeft();
+	virtual void KeyRight();
+
+private:
+	FwGui::ImageButton* up;
+	FwGui::ImageButton* down;
+	FwGui::Button* buttons[NUM_BUTTONS];
+	SaveItem items[MAX_ITEMS];
+	int numItems;
+	int scrollOffset;
+};
